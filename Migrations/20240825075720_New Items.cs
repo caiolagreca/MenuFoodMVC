552fc8315e06d4e01e00c9b0812c500a2b8445a0
Caio Lagreca <caiomiranda701@hotmail.com>
2024-08-25 18:37:16 +1000

    first commit

---
+++ Migrations/20240825075720_New Items.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MenuMVC.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
